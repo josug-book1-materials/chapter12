274e63684f9b7ca36896e88b6eceaff7214e3535
Nobuteru Nishida <nobuteru.nishida@ctc-g.co.jp>
2014-12-05 23:44:51 +0900

    update for openstack

--- lib/server.rb
+++ lib/server.rb
@@ -19,12 +19,12 @@ class Server
 
   attr_accessor :provider
   attr_accessor :name
-  attr_accessor :flavor_id
+  attr_accessor :flavor
   attr_accessor :image_id
   attr_accessor :key_name
   attr_accessor :key_file
   attr_accessor :availability_zone
-  attr_accessor :network
+  attr_accessor :network_id
   attr_accessor :security_groups
   attr_accessor :role
   attr_reader :ip_address
@@ -32,12 +32,12 @@ class Server
   def initialize(server_option = {})
     @provider = server_option["provider"].capitalize
     @name = server_option["name"]
-    @flavor_id = server_option["flavor_id"]
+    @flavor = server_option["flavor"]
     @image_id = server_option["image_id"]
     @key_name = server_option["key_name"]
     @key_file = server_option["key_file"]
     @availability_zone = server_option["availability_zone"]
-    @network = server_option["network"]
+    @network_id = server_option["network_id"]
     @security_groups = server_option["security_groups"] || []
     @role = server_option["role"]
     @ip_address = nil
@@ -49,17 +49,17 @@ module OpenstackApi
   def create
     api.servers.create(
       :name              => @name,
-      :flavor_ref        => @flavor_id,
+      :flavor_ref        => flavor_id,
       :image_ref         => @image_id,
       :key_name          => @key_name,
       :availability_zone => @availability_zone,
-      :nics              => [{ "net_id" => @network }],
+      :nics              => [{ "net_id" => @network_id }],
       :security_groups   => @security_groups
     )
   end
 
   def ip_address
-    @ip_address ||= (n=api.servers.find { |s| s.name == @name }.addresses[@network]) && n[0]['addr']
+    @ip_address ||= (n=api.servers.find { |s| s.name == @name }.addresses[network]) && n[0]['addr']
   end
 
   private
@@ -73,17 +73,36 @@ module OpenstackApi
         :openstack_region   => ENV['OS_REGION_NAME'] || ""
     })
   end
+
+  def network_api
+    Fog::Network.new({
+        :provider            => @provider,
+        :openstack_auth_url  => ENV['OS_AUTH_URL'] || "",
+        :openstack_username  => ENV['OS_USERNAME'] || "",
+        :openstack_tenant    => ENV['OS_TENANT_NAME'] || "",
+        :openstack_api_key   => ENV['OS_API_KEY'] || "",
+        :openstack_region    => ENV['OS_REGION_NAME'] || "",
+    })
+  end
+
+  def flavor_id
+    api.flavors.find { |f| f.name == @flavor }.id
+  end
+
+  def network
+    network_api.networks.all(:id => @network_id).first.name
+  end
 end
 
 module AwsApi
   def create
     api.servers.create(
       :tags => {'Name' => @name},
-      :flavor_id => @flavor_id,
+      :flavor_id => @flavor,
       :image_id => @image_id,
       :key_name => @key_name,
       :availablity_zone => @availability_zone,
-      :subnet_id => @network,
+      :subnet_id => @network_id,
       :security_group_ids => security_group_ids
     )
   end
