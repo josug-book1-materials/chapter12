08c681f4fce282478c6c139ea49b241f7d261fe3
Akihiro Motoki <amotoki@gmail.com>
2014-12-08 20:58:46 +0900

    Add scripts to prepare/cleanup fog-server

---
+++ cleanup_fog_server.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+
+WORK_DIR=$HOME/work_chapter
+
+get_uuid () { cat - | grep " id " | awk '{print $4}'; }
+
+for server in `nova list | grep " fog-server " | awk '{print $2}'`; do
+  nova delete $server
+done
+while true; do
+  nova list | grep -q " fog-server "
+  if [ $? -ne 0 ]; then
+    break
+  fi
+done
+
+for subnet in `neutron subnet-list | grep " fog-subnet " | awk '{print $2}'`; do
+  neutron router-interface-delete Ext-Router $subnet
+done
+for net in `neutron net-list | grep " fog-net " | awk '{print $2}'`; do
+  neutron net-delete $net
+done
+for sg in `neutron security-group-list | grep " sg-for-fog " | awk '{print $2}'`; do
+  neutron security-group-delete $sg
+done
+for key in `nova keypair-list | grep key-openstack | awk '{print $2}'`; do
+  nova keypair-delete key-openstack
+done
+
+for fip in `nova floating-ip-list | awk '{if ($6 == "-") print $2;}'`; do
+  nova floating-ip-delete $fip
+done
+
+echo "### keypair"
+nova keypair-list
+echo "### servers"
+nova list
+echo "### network"
+neutron net-list
+neutron subnet-list
+echo "### security group"
+neutron security-group-list
+echo "### floating IP"
+nova floating-ip-list
+
+#MY_FOG_NET=`neutron net-show fog-net | get_uuid`
+#FIP=`nova floating-ip-create | awk '{if ($6 == "-") print $2}'`
+#nova floating-ip-associate fog-server $FIP
---
+++ prepare_fog_server.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -x
+
+WORK_DIR=$HOME/work_chapter
+
+get_uuid () { cat - | grep " id " | awk '{print $4}'; }
+
+nova keypair-add key-openstack | tee $WORK_DIR/key-openstack.pem
+chmod 600 $WORK_DIR/key-openstack.pem
+
+neutron net-create fog-net
+neutron subnet-create --name fog-subnet fog-net 192.168.1.0/24
+neutron router-interface-add Ext-Router fog-subnet
+
+neutron security-group-create sg-for-fog
+neutron security-group-rule-create --protocol tcp \
+  --port-range-min 22 --port-range-max 22 \
+  --remote-ip-prefix 0.0.0.0/0 \
+  sg-for-fog
+neutron security-group-rule-create --protocol icmp \
+  --remote-ip-prefix 0.0.0.0/0 \
+  sg-for-fog
+neutron security-group-rule-create --protocol icmp \
+  --remote-ip-prefix 192.168.1.0/24 \
+  sg-for-fog
+neutron security-group-rule-create --protocol tcp \
+  --remote-ip-prefix 192.168.1.0/24 \
+  sg-for-fog
+
+MY_FOG_NET=`neutron net-show fog-net | get_uuid`
+nova boot \
+  --image centos-base \
+  --key-name key-openstack \
+  --flavor standard.medium \
+  --nic net-id=$MY_FOG_NET \
+  --security-groups sg-for-fog \
+  --availability-zone az1 \
+  fog-server
+
+TIMEOUT=120
+INTERVAL=5
+MAX_LOOP=`expr 120 / 5`
+for i in `seq 0 $MAX_LOOP`; do
+  nova list | grep fog-server | grep ACTIVE >/dev/null
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  if [ $i -eq $MAX_LOOP ]; then
+    echo "fog-server does not become ACTIVE in $TIMEOUT seconds"
+    exit 1
+  fi
+  sleep $INTERVAL
+done
+
+FIP=`nova floating-ip-create | awk '{if ($6 == "-") print $2}'`
+nova floating-ip-associate fog-server $FIP
+
+echo "### fog-server ($FIP) is launched."
