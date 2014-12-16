e8f903f5d1fd2f98b5001be9c47f78360e38fb67
n-nishida <nobuteru.nishida@ctc-g.co.jp>
2014-12-16 06:43:12 +0000

    move chef user variables to fogrc

--- deploy_app.rb
+++ deploy_app.rb
@@ -2,8 +2,5 @@ $:.unshift File.dirname(__FILE__)
 require 'lib/server'
 require 'lib/deployer'
 
-ENV['CHEF_REPO'] = ENV['HOME'] + "/chapter12/chef-repo-sample-app"
-ENV['COOKBOOK'] = "sample-app"
-
 servers = Server.create_from_file(ARGV[0])
 Deployer.new(servers).deploy()
